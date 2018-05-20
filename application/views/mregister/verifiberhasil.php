037e0b825cd8f8852ee4df38494d9c4478aa596d
Lufri Rais Maulana <lufriraismaulana@gmail.com>
2018-05-20 09:23:39 +0700

    WEB SERVICE Register Android

---
+++ application/views/mregister/verifiberhasil.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Multicraft Business</title>
+</head>
+<body>
+<h1>, Akunmu Telah Aktif silakan login di aplikasi android.</h1>
+</body>
+</html>
---
+++ application/views/mregister/verifigagal.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Multicraft business</title>
+</head>
+<body>
+	<h1>Verifikasi GAGAL</h1>
+</body>
+</html>
